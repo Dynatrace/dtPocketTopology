7c298837b293dc6515b8f6b971f5cf6d4a5f1aa4
Lucas Hocker <lucas.hocker@dynatrace.com>
2018-01-29 12:34:40 -0600

    Initial Commit

---
+++ dtPocketTopology/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dtPocketTopology
+//
+//  Created by Hocker, Lucas on 1/29/18.
+//  Copyright © 2018 Hocker, Lucas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
